07c0cf596f3c296df54c42569f2e1f427ee70746
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-11-02 13:12:56 +0500

    Experimenting With Themes

--- flutter_final_project/lib/main.dart
+++ flutter_final_project/lib/main.dart
@@ -9,32 +9,160 @@ class MyApp extends StatelessWidget
 {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) 
   {
     return MaterialApp
     (
-      title: 'Flutter Demo',
+      title: 'Experimenting With Themes',
       theme: ThemeData
       (
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        brightness: Brightness.light,
+        textTheme: TextTheme
+        (
+          // TextStyle? displayLarge,
+          // TextStyle? displayMedium,
+          // TextStyle? displaySmall,
+          // TextStyle? headlineLarge,
+          // TextStyle? headlineMedium,
+          // TextStyle? headlineSmall,
+          // TextStyle? titleLarge,
+          // TextStyle? titleMedium,
+          // TextStyle? titleSmall,
+          // TextStyle? bodyLarge,
+          // TextStyle? bodyMedium,
+          // TextStyle? bodySmall,
+          // TextStyle? labelLarge,
+          // TextStyle? labelMedium,
+          // TextStyle? labelSmall,
+          // TextStyle? headline1,
+          // TextStyle? headline2,
+          // TextStyle? headline3,
+          // TextStyle? headline4,
+          // TextStyle? headline5,
+          // TextStyle? headline6,
+          // TextStyle? subtitle1,
+          // TextStyle? subtitle2,
+          // TextStyle? bodyText1,
+          // TextStyle? bodyText2,
+          // TextStyle? caption,
+          // TextStyle? button,
+          // TextStyle? overline,
+        ),
+        appBarTheme: AppBarTheme
+        (
+          // Color? color,
+          // Color? backgroundColor,
+          // Color? foregroundColor,
+          // double? elevation,
+          // double? scrolledUnderElevation,
+          // Color? shadowColor,
+          // Color? surfaceTintColor,
+          // ShapeBorder? shape,
+          // IconThemeData? iconTheme,
+          // IconThemeData? actionsIconTheme,
+          // bool? centerTitle,
+          // double? titleSpacing,
+          // double? toolbarHeight,
+          // TextStyle? toolbarTextStyle,
+          // TextStyle? titleTextStyle,
+          // SystemUiOverlayStyle? systemOverlayStyle,
+        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // bool? applyElevationOverlayColor,
+        // NoDefaultCupertinoThemeData? cupertinoOverrideTheme,
+        // Iterable<ThemeExtension<dynamic>>? extensions,
+        // InputDecorationTheme? inputDecorationTheme,
+        // MaterialTapTargetSize? materialTapTargetSize,
+        // PageTransitionsTheme? pageTransitionsTheme,
+        // TargetPlatform? platform,
+        // ScrollbarThemeData? scrollbarTheme,
+        // InteractiveInkFeatureFactory? splashFactory,
+        // bool? useMaterial3,
+        // VisualDensity? visualDensity,
+        // Brightness? brightness,
+        // Color? canvasColor,
+        // Color? cardColor,
+        // ColorScheme? colorScheme,
+        // Color? colorSchemeSeed,
+        // Color? dialogBackgroundColor,
+        // Color? disabledColor,
+        // Color? dividerColor,
+        // Color? focusColor,
+        // Color? highlightColor,
+        // Color? hintColor,
+        // Color? hoverColor,
+        // Color? indicatorColor,
+        // Color? primaryColor,
+        // Color? primaryColorDark,
+        // Color? primaryColorLight,
+        // MaterialColor? primarySwatch,
+        // Color? scaffoldBackgroundColor,
+        // Color? secondaryHeaderColor,
+        // Color? shadowColor,
+        // Color? splashColor,
+        // Color? unselectedWidgetColor,
+        // String? fontFamily,
+        // List<String>? fontFamilyFallback,
+        // String? package,
+        // IconThemeData? iconTheme,
+        // IconThemeData? primaryIconTheme,
+        // TextTheme? primaryTextTheme,
+        // TextTheme? textTheme,
+        // Typography? typography,
+        // ActionIconThemeData? actionIconTheme,
+        // AppBarTheme? appBarTheme,
+        // BadgeThemeData? badgeTheme,
+        // MaterialBannerThemeData? bannerTheme,
+        // BottomAppBarTheme? bottomAppBarTheme,
+        // BottomNavigationBarThemeData? bottomNavigationBarTheme,
+        // BottomSheetThemeData? bottomSheetTheme,
+        // ButtonBarThemeData? buttonBarTheme,
+        // ButtonThemeData? buttonTheme,
+        // CardTheme? cardTheme,
+        // CheckboxThemeData? checkboxTheme,
+        // ChipThemeData? chipTheme,
+        // DataTableThemeData? dataTableTheme,
+        // DatePickerThemeData? datePickerTheme,
+        // DialogTheme? dialogTheme,
+        // DividerThemeData? dividerTheme,
+        // DrawerThemeData? drawerTheme,
+        // DropdownMenuThemeData? dropdownMenuTheme,
+        // ElevatedButtonThemeData? elevatedButtonTheme,
+        // ExpansionTileThemeData? expansionTileTheme,
+        // FilledButtonThemeData? filledButtonTheme,
+        // FloatingActionButtonThemeData? floatingActionButtonTheme,
+        // IconButtonThemeData? iconButtonTheme,
+        // ListTileThemeData? listTileTheme,
+        // MenuBarThemeData? menuBarTheme,
+        // MenuButtonThemeData? menuButtonTheme,
+        // MenuThemeData? menuTheme,
+        // NavigationBarThemeData? navigationBarTheme,
+        // NavigationDrawerThemeData? navigationDrawerTheme,
+        // NavigationRailThemeData? navigationRailTheme,
+        // OutlinedButtonThemeData? outlinedButtonTheme,
+        // PopupMenuThemeData? popupMenuTheme,
+        // ProgressIndicatorThemeData? progressIndicatorTheme,
+        // RadioThemeData? radioTheme,
+        // SearchBarThemeData? searchBarTheme,
+        // SearchViewThemeData? searchViewTheme,
+        // SegmentedButtonThemeData? segmentedButtonTheme,
+        // SliderThemeData? sliderTheme,
+        // SnackBarThemeData? snackBarTheme,
+        // SwitchThemeData? switchTheme,
+        // TabBarTheme? tabBarTheme,
+        // TextButtonThemeData? textButtonTheme,
+        // TextSelectionThemeData? textSelectionTheme,
+        // TimePickerThemeData? timePickerTheme,
+        // ToggleButtonsThemeData? toggleButtonsTheme,
+        // TooltipThemeData? tooltipTheme,
+        // AndroidOverscrollIndicator? androidOverscrollIndicator,
+        // Color? toggleableActiveColor,
+        // Color? selectedRowColor,
+        // Color? errorColor,
+        // Color? backgroundColor,
+        // Color? bottomAppBarColor,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -44,15 +172,6 @@ class MyApp extends StatelessWidget
 class MyHomePage extends StatefulWidget 
 {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -68,11 +187,6 @@ class _MyHomePageState extends State<MyHomePage>
   {
     setState(() 
     {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -80,43 +194,17 @@ class _MyHomePageState extends State<MyHomePage>
   @override
   Widget build(BuildContext context) 
   {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold
     (
       appBar: AppBar
       (
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center
       (
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column
         (
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>
           [
@@ -137,7 +225,7 @@ class _MyHomePageState extends State<MyHomePage>
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
