2404f5732feafd2a2503b97c1d72b50f4649857b
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-11-03 17:39:14 +0500

    Still experimenting with themes

--- flutter_final_project/lib/main.dart
+++ flutter_final_project/lib/main.dart
@@ -536,6 +536,34 @@ class _MyHomePageState extends State<MyHomePage>
                   );
                 },
               ),
+              TextButton
+              (
+                onPressed: () => showDialog<String>
+                (
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog
+                  (
+                    title: const Text('AlertDialog Title'),
+                    content: const Text('AlertDialog description'),
+                    actions: <Widget>
+                    [
+                      TextButton
+                      (
+                        onPressed: () => Navigator.pop(context, 'Cancel'),
+                        child: const Text('Cancel'),
+                      ),
+                      TextButton
+                      (
+                        onPressed: () => Navigator.pop(context, 'OK'),
+                        child: const Text('OK'),
+                      ),
+                    ],
+                  ),
+                ),
+                child: const Text('Show Dialog'),
+              ),
+
+
             ],
           ),
         ),
