8d6578ad62c414b14a691fbd50535782cc7d1aad
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-11-05 10:26:30 +0500

    Still experimenting with themes

--- flutter_final_project/lib/main.dart
+++ flutter_final_project/lib/main.dart
@@ -168,7 +168,8 @@ class MyApp extends StatelessWidget
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
       // home: DatePickerExample(),
       // home: PopupMenuExample(),
-      home: RadioExample(),
+      // home: RadioExample(),
+      home: SliderExample(),
     );
   }
 }
@@ -1032,3 +1033,40 @@ class _RadioExampleState extends State<RadioExample>
     );
   }
 }
+
+class SliderExample extends StatefulWidget 
+{
+  const SliderExample({super.key});
+
+  @override
+  State<SliderExample> createState() => _SliderExampleState();
+}
+
+class _SliderExampleState extends State<SliderExample> 
+{
+  double _currentSliderValue = 20;
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Scaffold
+    (
+      appBar: AppBar(title: const Text('Slider')),
+      body: Slider
+      (
+        value: _currentSliderValue,
+        max: 100,
+        divisions: 5,
+        label: _currentSliderValue.round().toString(),
+        onChanged: (double value) 
+        {
+          setState(() 
+          {
+            _currentSliderValue = value;
+          });
+        },
+      ),
+    );
+  }
+}
+
