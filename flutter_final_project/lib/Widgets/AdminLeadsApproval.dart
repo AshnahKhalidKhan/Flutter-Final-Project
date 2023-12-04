50ab8d03dc7749288722d737cbb0d40c57cdc5ab
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-12-04 23:46:59 +0500

    Still making GDSC Leads Approval Page

---
+++ flutter_final_project/lib/Widgets/AdminLeadsApproval.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GDSCLeadsApproval extends StatefulWidget 
+{
+  const GDSCLeadsApproval({super.key});
+
+  @override
+  State<GDSCLeadsApproval> createState() => _GDSCLeadsApprovalState();
+}
+
+class _GDSCLeadsApprovalState extends State<GDSCLeadsApproval> 
+{
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
