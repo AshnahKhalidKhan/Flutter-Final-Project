7051b03e8bc5ddd292d6270834700ce397d8a852
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-12-19 01:40:46 +0500

    Firebase connection complete! (Alhumdulillah!!!)

--- flutter_final_project/lib/main.dart
+++ flutter_final_project/lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_final_project/Widgets/AdminLeadsApproval.dart';
 import 'package:flutter_final_project/Widgets/Gallery.dart';
 import 'package:flutter_final_project/Widgets/Profile.dart';
 import 'package:flutter_final_project/Widgets/SignUp.dart';
+import 'package:flutter_final_project/firebase_options.dart';
 
-void main() 
+void main() async
 {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp
+  (
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
