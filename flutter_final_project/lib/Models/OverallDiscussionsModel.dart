1c2240bb94fbf195864c9ceb371cfdc2a72ef697
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-12-20 23:25:09 +0500

    Created OverallDiscussions model file

---
+++ flutter_final_project/lib/Models/OverallDiscussionsModel.dart
@@ -0,0 +1,13 @@
+class OverallDiscussion {
+  final String discussionId;
+  final String message;
+  final String senderId;
+  final DateTime timestamp;
+
+  OverallDiscussion({
+    required this.discussionId,
+    required this.message,
+    required this.senderId,
+    required this.timestamp,
+  });
+}
