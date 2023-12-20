ffad6be66366165955532e66e2c12683c892ccbf
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-12-20 23:22:09 +0500

    Created Registrations model file

---
+++ flutter_final_project/lib/Models/RegistrationsModel.dart
@@ -0,0 +1,11 @@
+class Registration {
+  final String eventId;
+  final String userId;
+  final String registrationDetails;
+
+  Registration({
+    required this.eventId,
+    required this.userId,
+    required this.registrationDetails,
+  });
+}
