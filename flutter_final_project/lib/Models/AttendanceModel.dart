9003919e5000006f669bbb1d85c90e930bd64087
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-12-20 23:22:37 +0500

    Created  Attendance model file

---
+++ flutter_final_project/lib/Models/AttendanceModel.dart
@@ -0,0 +1,11 @@
+class Attendance {
+  final String eventId;
+  final String userId;
+  final bool present;
+
+  Attendance({
+    required this.eventId,
+    required this.userId,
+    required this.present,
+  });
+}
