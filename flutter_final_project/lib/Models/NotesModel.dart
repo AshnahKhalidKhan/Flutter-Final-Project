0cdba756a59e0f0723addb8de76daa1e3f0adc82
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-12-20 23:21:30 +0500

    Created notes model file

---
+++ flutter_final_project/lib/Models/NotesModel.dart
@@ -0,0 +1,13 @@
+class Note {
+  final String eventId;
+  final String message;
+  final String senderId;
+  final DateTime timestamp;
+
+  Note({
+    required this.eventId,
+    required this.message,
+    required this.senderId,
+    required this.timestamp,
+  });
+}
