840b4bee59694e36f9ac72fde258b41b86c92bf6
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-12-20 23:24:23 +0500

    Created EventMedia model file

---
+++ flutter_final_project/lib/Models/EventMediaModel.dart
@@ -0,0 +1,13 @@
+class EventMedia {
+  final String eventId;
+  final String mediaType;
+  final String mediaURL;
+  final String uploadedBy;
+
+  EventMedia({
+    required this.eventId,
+    required this.mediaType,
+    required this.mediaURL,
+    required this.uploadedBy,
+  });
+}
