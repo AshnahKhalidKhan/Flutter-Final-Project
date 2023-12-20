fe3d4c8e4d9b5bd8399802eb905d70ef452fb739
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-12-20 23:18:22 +0500

    Created User Model

---
+++ flutter_final_project/lib/Models/UserModel.dart
@@ -0,0 +1,17 @@
+class User {
+  final String userId;
+  final String name;
+  final String email;
+  final String role;
+  final bool approved;
+  final String campus;
+
+  User({
+    required this.userId,
+    required this.name,
+    required this.email,
+    required this.role,
+    required this.approved,
+    required this.campus,
+  });
+}
