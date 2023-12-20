de506871ee47a815b0c5ac429098cd096fa5ebf4
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-12-20 23:20:59 +0500

    Created tasks model file

---
+++ flutter_final_project/lib/Models/TaskModel.dart
@@ -0,0 +1,13 @@
+class Task {
+  final String taskId;
+  final String eventId;
+  final String task;
+  final bool completed;
+
+  Task({
+    required this.taskId,
+    required this.eventId,
+    required this.task,
+    required this.completed,
+  });
+}
