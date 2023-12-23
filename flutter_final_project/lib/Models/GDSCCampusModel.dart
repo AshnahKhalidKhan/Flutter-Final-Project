d66b5e14b02463aa2a9bc03de17931ab891df919
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-12-23 23:06:41 +0500

    Fixing model files

---
+++ flutter_final_project/lib/Models/GDSCCampusModel.dart
@@ -0,0 +1,31 @@
+class GDSCCampus 
+{
+  final String id;
+  final String name;
+  final String email;
+  final String location;
+  final bool lead;
+  final String facebookEmail;
+  final String facebookPassword;
+  final String instagramEmail;
+  final String instagramPassword;
+  final String linkedInEmail;
+  final String linkedInPassword;
+
+  GDSCCampus
+  (
+    {
+      required this.id,
+      required this.name,
+      required this.email,
+      required this.location,
+      required this.lead,
+      required this.facebookEmail,
+      required this.facebookPassword,
+      required this.instagramEmail,
+      required this.instagramPassword,
+      required this.linkedInEmail,
+      required this.linkedInPassword,
+    }
+  );
+}
---
+++ flutter_final_project/lib/Models/GDSCMemberModel.dart
@@ -0,0 +1,19 @@
+class GDSCMember 
+{
+  final String id;
+  final String name;
+  final String email;
+  final String campus;
+  final bool approved;
+
+  GDSCMember
+  (
+    {
+      required this.id,
+      required this.name,
+      required this.email,
+      required this.campus,
+      required this.approved,
+    }
+  );
+}
