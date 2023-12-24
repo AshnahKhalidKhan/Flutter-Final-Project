a706b5d7d263ea18fabde30976c153f48b38221b
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-12-24 23:22:47 +0500

    Firebase bro, bismillah bismillah

--- flutter_final_project/lib/Models/NotesModel.dart
+++ flutter_final_project/lib/Models/NotesModel.dart
@@ -1,13 +1,17 @@
-class Note {
+class Note 
+{
   final String eventId;
   final String message;
   final String senderId;
   final DateTime timestamp;
 
-  Note({
-    required this.eventId,
-    required this.message,
-    required this.senderId,
-    required this.timestamp,
-  });
+  Note
+  (
+    {
+      required this.eventId,
+      required this.message,
+      required this.senderId,
+      required this.timestamp,
+    }
+  );
 }
