cd7b41df484ebdb5e991e1a8a2a1744fee370dbf
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-12-25 13:14:51 +0500

    Usermodel all methods added

--- flutter_final_project/lib/Core/Repositories/AuthenticationRepository.dart
+++ flutter_final_project/lib/Core/Repositories/AuthenticationRepository.dart
@@ -1,57 +1,67 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class AuthRepository {
+class AuthRepository 
+{
   final FirebaseFirestore firebaseFirestore;
-  final  Firebase
+  final FirebaseAuth firebaseAuth;
 
-  AuthRepository({
-    required this.firebaseFirestore,
-    required this.firebaseAuth,
-  });
-
-  Stream<fb_auth.User?> get user => firebaseAuth.userChanges();
-
-  Future<void> signInWithEmail({
-    required String email,
-    required String password,
-  }) async {
-    try {
-      await firebaseAuth.signInWithEmailAndPassword(
-          email: email, password: password);
-    } on fb_auth.FirebaseAuthException catch (e) {
-      throw CustomError(code: e.code, message: e.message!, plugin: e.plugin);
-    } catch (e) {
-      throw CustomError(
-        code: 'Exception',
-        message: e.toString(),
-        plugin: 'flutter_error/server_error',
-      );
+  AuthRepository
+  (
+    {
+      required this.firebaseFirestore,
+      required this.firebaseAuth,
     }
+  );
+
+  //CREATE, READ, UPDATE, DELETE
+
+  Future<void> signOut() async 
+  {
+    await firebaseAuth.signOut();
   }
 
-  Future<void> signUpWithEmail({
-    required String firstname,
-    required String lastname,
-    required String gender,
-    required String email,
-    required String password,
-  }) async {
-    try {
-      final fb_auth.UserCredential userCredentail =
-          await firebaseAuth.createUserWithEmailAndPassword(
+  Future<void> signUpWithEmail
+  (
+    {
+      required String name,
+      required String email,
+      required String password,
+      required String role,
+      String? campus
+    }
+  )
+  async
+  {
+    try 
+    {
+      final UserCredential userCredential = await firebaseAuth.createUserWithEmailAndPassword
+      (
         email: email,
-        password: password,
+        password: password
       );
+      final User signedInUser = userCredential.user!;
+      User user;
+      switch (role)
+      {
+        case 'member':
+        {
+          user = User
+          (
 
-      final signedInUser = userCredentail.user!;
-      User user = User(
+          );
+          break;
+        }
+      }
+      User user = User
+      (
         id: signedInUser.uid,
-        firstname: firstname,
-        lastname: lastname,
-        gender: gender,
-        username: '',
+        name: name,
         email: email,
+        role: 'member',
+        campus: campus,
+        approved: false
       );
       await usersRef.doc(signedInUser.uid).set(user.toJson());
     } on fb_auth.FirebaseAuthException catch (e) {
@@ -63,9 +73,5 @@ class AuthRepository {
         plugin: 'flutter_error/server_error',
       );
     }
-  }
-
-  Future<void> signOut() async {
-    await firebaseAuth.signOut();
   }
 }
--- flutter_final_project/lib/Models/UserModel.dart
+++ flutter_final_project/lib/Models/UserModel.dart
@@ -1,4 +1,7 @@
-class User 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable
 {
   final String id;
   final String name;
@@ -7,15 +10,98 @@ class User
   final String? campus;
   final bool? approved;
 
-  User
+  const User
   (
     {
       required this.id,
       required this.name,
       required this.email,
       required this.role,
-      required this.campus,
+      this.campus,
       required this.approved
     }
   );
+
+  Map<String, dynamic> toJson() =>
+  {
+    'id': id,
+    'name': name, 
+    'email': email,
+    'role': role,
+    'campus': campus,
+    'approved': approved
+  };
+
+  static User fromJson(Map<String, dynamic> json) 
+  {
+    return User
+    (
+      id: json['id'],
+      name: json['name'], 
+      email: json['email'], 
+      role: json['role'],
+      campus: json['campus'], 
+      approved: json['approved']
+    );
+  }
+
+  factory User.fromDoc(DocumentSnapshot userDoc)
+  {
+    final userData = userDoc.data() as Map<String, dynamic>?;
+    return User
+    (
+      id: userData!['id'],
+      name: userData!['name'], 
+      email: userData!['email'], 
+      role: userData!['role'],
+      campus: userData!['campus'], 
+      approved: userData!['approved']
+    );
+  }
+
+  factory User.initial()
+  {
+    return const User
+    (
+      id: '',
+      name: '', 
+      email: '',
+      role: '',
+      campus: '',
+      approved: false
+    );
+  }
+
+  @override
+  bool get stringify => true;
+
+  @override
+  List<Object?> get props 
+  {
+    return [id, name, email, role, campus, approved];
+  }
+  
+  User copyWith
+  (
+    {
+      String? id,
+      String? name,
+      String? email,
+      String? role,
+      String? campus,
+      bool? approved
+    }
+  )
+  {
+    return User
+    (
+      id: id ?? this.id,
+      name: name ?? this.name,
+      email: email ?? this.email,
+      role: role ?? this.role,
+      campus: campus ?? this.campus,
+      approved: approved ?? this.approved
+    );
+  }
+  
 }
